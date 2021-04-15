ee956b4c57a572f4ce637331722b06d1b655857e
Jongkeun Choi <jongkeun.ch@gmail.com>
2021-04-15 14:41:18 +0900

    feat: twitter 로그인 및 로그인 확인 지원

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-
 var express = require("express");
 var passport = require("passport");
 var Strategy = require("passport-twitter").Strategy;
@@ -18,7 +17,7 @@ passport.use(
       callbackURL:
         process.env.NODE_ENV === "production"
           ? "https://roco.moe/callback_twitter.html"
-          : "http://localhost:8081/oauth/callback",
+          : "http://localhost:8081/auth/twitter/callback",
     },
     function (token, tokenSecret, profile, cb) {
       return cb(null, profile);
@@ -27,33 +26,74 @@ passport.use(
 );
 
 var app = express();
-
 app.use(require("morgan")("combined"));
 app.use(require("body-parser").urlencoded({ extended: true }));
 app.use(
   require("express-session")({
     secret: process.env["SESSION_SECRET"],
     resave: true,
-    saveUninitialized: true,
+    saveUninitialized: false,
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get("/login/twitter", passport.authenticate("twitter"));
-
+app.get("/auth/twitter", passport.authenticate("twitter"));
 app.get(
-  "/oauth/callback",
-  passport.authenticate("twitter", { failureRedirect: "/login" }),
+  "/auth/twitter/callback",
+  passport.authenticate("twitter"),
   function (req, res) {
-    res.redirect("/profile/twitter");
+    const { id, displayName, photos } = req.user;
+    console.log(req.user);
+    res.send(
+      renderTemplate("twitter", {
+        id,
+        displayName,
+        photo: photos[0] ? photos[0].value : null,
+      })
+    );
   }
 );
 
-app.get("/profile/twitter", function (req, res) {
-  console.log(req.user);
-  res.json({ user: req.user });
+app.get("/auth/twitter/check", function (req, res) {
+  if (!req.user) {
+    res.json({ isLogin: false });
+    return;
+  }
+  const { id } = req.user;
+  if (req.query.id !== id) {
+    res.json({ isLogin: false });
+    return;
+  }
+  res.json({ isLogin: true });
 });
+
+function renderTemplate(authMethod, authValue) {
+  const services = new Map();
+  services.set("twitter", "트위터");
+  const targetOrigin =
+    process.env.NODE_ENV === "production"
+      ? "https://roco.moe"
+      : "http://localhost:3000";
+  if (!services.has(authMethod)) return ``;
+  return `<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8" />
+      <title>${services.get(authMethod)} 로그인</title>
+    </head>
+    <body>
+      <h1>로그인 중이에요...</h1>
+      <script type="text/javascript">
+        const user = ${JSON.stringify(authValue).replace(/</g, "\\u003c")};
+        console.log(user);
+        const message = { authMethod: "${authMethod}", authValue: user};
+        window.opener.postMessage(message, "${targetOrigin}");
+      </script>
+    </body>
+  </html>
+  `;
+}
 
 app.listen(process.env["PORT"] || 8081);
